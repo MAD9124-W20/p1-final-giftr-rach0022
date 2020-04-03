e589397baa2d88a5f47d197bc0e18cf8261e95c2
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-04-03 17:43:05 -0400

    updated logerrors middleware to use winston

--- middleware/logErrors.js
+++ middleware/logErrors.js
@@ -1,7 +1,7 @@
 //log the errors with this handy middleware
 //instead of always throwing it in the catch part of a try catch
-
+logger = require('../startup/logger.js');
 module.exports = async (err, req, res, next) =>  {
-    console.error(err.stack);
+    logger.log('error', err.stack);
     next(err);
 }
